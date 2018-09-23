03dfeb3ac8724bb464af61f15ca7d7326550136d
Cutlasses <spitkethly@gmail.com>
2018-09-23 17:39:18 +0100

    Adding I2C
    Not working quite right..

--- src/ButtonStrip.c
+++ src/ButtonStrip.c
@@ -21,17 +21,17 @@
 #pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
 
-#define I2C_ADDRESS 111
-#define DATA_SIZE_BYTES 12
-#define DATA_SIZE_WORDS 6
+#define I2C_ADDRESS 222
+//#define DATA_SIZE_BYTES 2
+//#define DATA_SIZE_WORDS 1
 
 #define NUM_LED_SWITCHES 8
 
 
 typedef unsigned char byte;
 
-byte led_values[NUM_LED_SWITCHES];
-byte switch_values[NUM_LED_SWITCHES];
+byte led_values;
+byte switch_values;
 
 void init_PIC()
 {
@@ -52,16 +52,80 @@ void init_PIC()
 	ADCON0      = 0;    
 }
 
+void interrupt ISR(void)
+{
+    //////////////////////////////////////////////////////////
+    // I2C SLAVE INTERRUPT
+    // Called when there is activity on the I2C bus
+    if( PIR1bits.SSP1IF )
+    {      
+        PIR1bits.SSP1IF = 0; // clear interrupt flag
+
+        if( !SSP1STATbits.D_nA ) // master has sent our slave address
+        {
+            byte d = SSP1BUF; // read and discard address to clear BF flag
+
+            // Is the master setting up a data READ?
+            if( SSP1STATbits.R_nW )
+            {      
+                // send the switch values
+                SSP1BUF = switch_values;
+            }
+            else
+            {
+                // read the led values
+                //led_values = SSP1BUF;
+                SSP1BUF = 0;
+            }
+        }
+        else // DATA (will this be reached if only read/writing a single byte)
+        {
+            if( SSP1STATbits.R_nW ) // MASTER IS READING FROM SLAVE
+            {
+                // send the switch values
+                SSP1CON1bits.WCOL = 0; // clear write collision bit
+                
+                SSP1BUF = switch_values;
+            }
+            else // MASTER IS WRITING TO SLAVE
+            {                                              
+                // read the led values
+                //led_values = SSP1BUF;
+            }
+        }
+        
+        SSP1CON1bits.CKP = 1; // release clock
+    }
+}
+
+////////////////////////////////////////////////////////////
+//
+// I2C SLAVE INITIALISATION
+//
+////////////////////////////////////////////////////////////
+
+void i2c_init( byte addr )
+{
+    SSP1CON1    	= 0b00100110;   // I2C slave mode, with 7 bit address, enable i2c
+    SSP1MSK     	= 0b01111111;   // address mask bits 0-6
+    SSP1ADD     	= addr<<1;      // set slave address
+    PIE1bits.SSP1IE	= 1;
+	PIR1bits.SSP1IF	= 0;
+    
+    // global enable interrupts
+	INTCONbits.GIE  = 1;
+    INTCONbits.PEIE = 1;
+}
+
 void main()
 {
+    led_values = 0;
+    switch_values = 0;
+    
     init_PIC();
     
-    for( int i = 0; i < NUM_LED_SWITCHES; ++i )
-    {
-        led_values[i]       = 1;
-        switch_values[i]    = 0;
-    }
-    
+    i2c_init( I2C_ADDRESS );
+       
     // clear the shift register
     PORTCbits.RC5 = 0;      // DATA
     for( int i = 0; i < 8; ++i )
@@ -94,10 +158,27 @@ void main()
             PORTCbits.RC4       = 0;      // STORE CLOCK
             PORTCbits.RC4       = 1;      // STORE CLOCK
  
-            switch_values[i]    = PORTAbits.RA5;
+            //switch_values[i]    = PORTAbits.RA5;
+            if( PORTAbits.RA5 )
+            {
+                switch_values   |= (PORTAbits.RA5 << i);
+            }
+            else
+            {
+                switch_values   &= ~(PORTAbits.RA5 << i);
+            }
             
+            led_values          = switch_values; // temp - set led on when switch is on
             //PORTCbits.RC2       = led_values[i]; // set RC2 high to turn on LEDs (sinking current through the transistor)
-            PORTCbits.RC2       = switch_values[i];
+            
+            if( led_values & (PORTAbits.RA5 << i) )
+            {
+                PORTCbits.RC2   = 1; // set RC2 high to turn on LEDs (sinking current through the transistor)
+            }
+            else
+            {
+                PORTCbits.RC2    = 0; // set RC2 low to turn off LEDs (transistor will not conduct)
+            }
 
             __delay_ms(1);
         }
